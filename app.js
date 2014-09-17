f334af81b87a56c85a3f18ed3eabfe8600fe39ff
Jordan Vishniac <jordan@vishniac.com>
2014-09-17 14:28:40 -0400

    Added variable called value

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+var value = 10;
+
 app.get('/', function (req, res) {
-	res.send('Jenkins Test');
+	res.send('');
 });
 
 app.listen(process.env.PORT || 5000);
