fb5a2e938915502ec1173c6ce71d5f045df43bf0
Jordan Vishniac <jordan@vishniac.com>
2014-09-17 14:12:31 -0400

    Changed message to 'Jenkins Tutorial'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send('Testing Jenkins/nHello World');
+	res.send('Jenkins Tutorial');
 });
 
 app.listen(process.env.PORT || 5000);
