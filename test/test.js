9eb53e2120a8c80d2c701cb9b238fa886b7c7157
Jordan Vishniac <jordan@vishniac.com>
2014-09-17 14:04:26 -0400

    Changed message a third time.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 	it('respond with Testing Jenkins', function(done) {
-		request(app).get('/').expect('Testing Jenkins/nHello Jenkins', done);
+		request(app).get('/').expect('Testing Jenkins/nHello World', done);
 	});
 });
